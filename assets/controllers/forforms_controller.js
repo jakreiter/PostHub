f776a1b5669c3c1c347d63c3abe83dfb43c9d7a8
Jakub <jaresz@phpd.eu>
2021-05-23 19:12:09 +0200

    layout

--- assets/controllers/forforms_controller.js
+++ assets/controllers/forforms_controller.js
@@ -3,7 +3,6 @@ import addLetterforms from './../js/addLetterforms';
 
 export default class extends Controller {
     connect() {
-        this.element.textContent = 'forforms Stimulus! Edit me in assets/controllers/hello_controller.js';
-        addLetterforms(2);
+        addLetterforms(3);
     }
 }
