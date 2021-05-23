2c831027977154c7abb2ba782a79e050e5a66ace
Jakub <jaresz@phpd.eu>
2021-05-23 14:57:56 +0200

    organization edit - ajax owning user select

--- src/Form/OrganizationType.php
+++ src/Form/OrganizationType.php
@@ -8,6 +8,8 @@ use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class OrganizationType extends AbstractType
@@ -36,6 +38,36 @@ class OrganizationType extends AbstractType
                     'class' => 'user-ajax-select'
                 )
             ])
+            ->addEventListener(
+                FormEvents::PRE_SUBMIT,
+                function (FormEvent $event) {
+                    $form = $event->getForm();
+
+                    $data = $event->getData();
+                    $submittedUserId = $data['owner'];
+                    if ($submittedUserId) {
+                        $form->add('owner', EntityType::class, [
+                            'class' => 'App:User',
+                            'query_builder' => function (UserRepository $er) use ($submittedUserId) {
+                                return $er->createQueryBuilder('o')
+                                    ->andWhere('o.id = :submittedUserId')
+                                    ->setParameter('submittedUserId', $submittedUserId);
+                            },
+                            'choice_label' => function (User $owner) {
+                                return $owner->getUsername().' ('.$owner->getEmail().')';
+                            },
+                            'multiple' => false,
+                            'expanded' => false,
+                            'required' => true,
+                            'placeholder' => '',
+                            'attr' => array(
+                                'class' => 'owner-ajax-select'
+                            )
+                        ]);
+                    }
+
+                }
+            );
         ;
     }
 
