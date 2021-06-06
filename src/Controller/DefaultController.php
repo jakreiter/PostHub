77d09afedbf262a7ebc3d95b68db2f0a79e8db60
Jakub <jaresz@phpd.eu>
2021-06-06 15:59:16 +0200

    default home page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,14 +6,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Form\LetterType;
-use App\Entity\Letter;
 use App\Entity\User;
-use App\Repository\UserRepository;
 use App\Repository\OrganizationRepository;
 use App\Repository\LetterRepository;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\Security as CoreSecurity;
+
 
 
 /**
@@ -27,29 +26,34 @@ class DefaultController extends AbstractController
      * @Security("is_granted('ROLE_USER')")
      */
 
-    public function homeAction(Request $request, SluggerInterface $slugger, LetterRepository $letterRepository, OrganizationRepository $organizationRepository): Response
+    public function homeAction(Request $request, SluggerInterface $slugger, LetterRepository $letterRepository, OrganizationRepository $organizationRepository, CoreSecurity $security): Response
     {
         /** @var User $user */
         $user = $this->getUser();
 
         $numberOfUnseenPerOrganization = [];
         $defaultOrganizationId = null;
-
-        $organizations = $user->getOrganizations();
-        if (count($organizations) > 1) {
-            foreach ($organizations as $organization) {
-                $numberOfUnseenPerOrganization[$organization->getId()] = $letterRepository->getNumberOfUnseenPerOrganization($organization);
-            }
-            $maxUnseen = max($numberOfUnseenPerOrganization);
-            foreach ($numberOfUnseenPerOrganization as $organizationId => $numberOfUnseen) {
-                if ($numberOfUnseen === $maxUnseen) $defaultOrganizationId = $organizationId;
-            }
-
-        } else if (1 === count($organizations)) {
-            $defaultOrganizationId = $organizations[0]->getId();
+        if ($security->isGranted('ROLE_LOCATION_MODERATOR')) {
+            // letter_admin_index for ADMIN
+            return $this->redirectToRoute('letter_admin_index', ['sort' => 'Letter.created', 'direction'=>'desc']);
         }
-        if ($defaultOrganizationId) {
-            return $this->redirectToRoute('letter_user_index', ['id' => $defaultOrganizationId]);
+        else {
+            $organizations = $user->getOrganizations();
+            if (count($organizations) > 1) {
+                foreach ($organizations as $organization) {
+                    $numberOfUnseenPerOrganization[$organization->getId()] = $letterRepository->getNumberOfUnseenPerOrganization($organization);
+                }
+                $maxUnseen = max($numberOfUnseenPerOrganization);
+                foreach ($numberOfUnseenPerOrganization as $organizationId => $numberOfUnseen) {
+                    if ($numberOfUnseen === $maxUnseen) $defaultOrganizationId = $organizationId;
+                }
+
+            } else if (1 === count($organizations)) {
+                $defaultOrganizationId = $organizations[0]->getId();
+            }
+            if ($defaultOrganizationId) {
+                return $this->redirectToRoute('letter_user_index', ['id' => $defaultOrganizationId]);
+            }
         }
         return $this->render('home.html.twig', []);
     }
