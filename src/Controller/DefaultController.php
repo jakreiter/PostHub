e3016fe37c502294dba345e694093c6ebcf97cf9
Jakub <jaresz@phpd.eu>
2021-11-18 18:55:21 +0100

    Redirect tp https if https is default scheme removed

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -28,10 +28,12 @@ class DefaultController extends AbstractController
 
     public function homeAction(Request $request, SluggerInterface $slugger, LetterRepository $letterRepository, OrganizationRepository $organizationRepository, CoreSecurity $security): Response
     {
+        /*
         if ('https'==$_ENV['WEB_APP_SCHEME'] && 'http'==$request->getScheme()) {
             $appUrl = $_ENV['WEB_APP_SCHEME'].'://'.$_ENV['WEB_APP_HOST'].$_ENV['WEB_APP_BASE_URL'];
             return $this->redirect($appUrl);
         }
+        */
 
         /** @var User $user */
         $user = $this->getUser();
